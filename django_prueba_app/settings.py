c8462a7ad770574faeaa42ac9340b4cd7dc5e374
unknown <u20152139849@usco.edu.co>
2024-10-22 13:27:11 -0500

    secrete_key

--- django_prueba_app/settings.py
+++ django_prueba_app/settings.py
@@ -13,7 +13,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+#SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-q3ms8_!)q#wvhfcsji(rm2i0tiid9evffm9bn837p@$)qywf%&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
